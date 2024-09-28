85a79639181d850ed66545d08783a5460fee2ec4
kpbhumika <kpbhumika@gmail.com>
2024-09-28 15:22:02 -0400

    Added Condiment class.

---
+++ src/main/java/edu/bu/met/cs665/assignment1/Condiment.java
@@ -0,0 +1,30 @@
+package edu.bu.met.cs665.assignment1;
+
+public class Condiment {
+
+    private String type;
+    private int units;
+    private double pricePerUnit;
+
+    public Condiment(String type, int units, double pricePerUnit) {
+        if (units < 0 || units > 3) {
+            throw new IllegalArgumentException("Units for "+ type + " must be between 0 and 3");
+        }
+        this.type = type;
+        this.units = units;
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getUnits() {
+        return units;
+    }
+
+    public double getPrice() {
+        return units * pricePerUnit;
+    }
+}
+
