bf4d860b7e73a73c8faba347227540d486bd56d6
kpbhumika <kpbhumika@gmail.com>
2024-09-28 15:25:26 -0400

    Added Milk and Sugar class to Condiment class.

---
+++ src/main/java/edu/bu/met/cs665/assignment1/Milk.java
@@ -0,0 +1,9 @@
+package edu.bu.met.cs665.assignment1;
+
+public class Milk extends Condiment {
+
+    public Milk(int units) {
+        super("Milk", units, 0.5);
+    }
+}
+
---
+++ src/main/java/edu/bu/met/cs665/assignment1/Sugar.java
@@ -0,0 +1,8 @@
+package edu.bu.met.cs665.assignment1;
+
+public class Sugar extends Condiment {
+
+    public Sugar(int units) {
+        super("Sugar", units, 0.5);
+    }
+}
